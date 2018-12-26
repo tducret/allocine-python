3e88e6dd6af629147bc10b50d344ba0e3f5f53f0
Thibault Ducret <thibault.ducret@gmail.com>
2018-12-26 16:15:46 +0100

    Update

--- allocine/__init__.py
+++ allocine/__init__.py
@@ -3,44 +3,113 @@
 """Top-level package for Allociné."""
 
 import requests
-from requests import ConnectionError
 from dataclasses import dataclass
 from typing import List
 from datetime import datetime
+from bs4 import BeautifulSoup
+from json import loads
 
 __author__ = """Thibault Ducret"""
 __email__ = 'hello@tducret.com'
 __version__ = '0.0.1'
 
 _DEFAULT_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f%z'
+_DEFAULT_BEAUTIFULSOUP_PARSER = "html.parser"
 
 
-class Client(object):
-    """ Do the requests with the servers """
-    def __init__(self):
-        self.session = requests.session()
-        self.headers = {
-                    'Host': 'myhost.com',
-                    'User-Agent': 'User agent',
+class Allocine:
+    def __init__(self, theater_id):
+        """ theater_id is the Allociné theater identifier.
+        It can be found in the showtimes url
+        http://www.allocine.fr/seance/salle_gen_csalle=[theater_id].html
+        For example : theater_id="C0159" for UGC Ciné Cité Les Halles"""
+        self.allocine_dict = self._get_showtimes_dict(theater_id=theater_id)
+        self.movies = self._get_movies(allocine_dict=self.allocine_dict)
+        self.theater = self._get_theater_info(allocine_dict=self.allocine_dict,
+                                              theater_id=theater_id)
+        self.showtimes = self._get_showtimes(theater_id=theater_id)
+
+    def _get_movie_obj_from_id(self, movie_id):
+        """ Returns a Movie object from its id """
+        found_movie = None
+        for movie in self.movies:
+            if movie.id == movie_id:
+                found_movie = movie
+                break
+        return found_movie
+
+    def _get_showtimes(self, theater_id):
+        """ From the Allociné dict, returns a list of Showtime objects
+        """
+        showtimes = []
+        showtimes_per_day = self.allocine_dict.get('showtimes').get(theater_id)
+        for date, movies in showtimes_per_day.items():
+            for movie_id, movie_versions in movies.items():
+                # movie_version is a kind of display for the movie
+                # (language, 3D/2D...)
+                movie_obj = self._get_movie_obj_from_id(movie_id=movie_id)
+
+                for movie_version in movie_versions:
+
+                    if movie_version.get("version") == "translated":
+                        version = "VF"
+                    else:
+                        version = "VOST"
+
+                    movie_version_obj = MovieVersion(title=movie_obj.title,
+                                                     id=movie_obj.id,
+                                                     version=version)
+
+                    for showtime in movie_version.get('showtimes'):
+
+                        showtime_obj = Showtime(
+                            datetime_str=showtime.get("movieStart"),
+                            movie=movie_version_obj)
+
+                        showtimes.append(showtime_obj)
+                        # print("{} : {}".format(
+                        #     movie_obj.title,
+                        #     showtime_obj.date))
+        return showtimes
+
+    @staticmethod
+    def _get_showtimes_dict(theater_id):
+        """ Get the `data-movies-showtimes` dict from Allociné webpage.
+        It contains all the information about the theater, the movies and
+        the showtimes.
+        Returns a dict."""
+        session = requests.session()
+        headers = {
+                    'Host': 'www.allocine.fr',
+                    'User-Agent': 'Mozilla/5.0 (Macintosh; \
+                                   Intel Mac OS X 10.14; rv:63.0) \
+                                   Gecko/20100101 Firefox/63.0',
                     }
+        url = "http://www.allocine.fr/seance/salle_gen_csalle={}.html".format(
+            theater_id)
+        ret = session.get(url=url, headers=headers)
+        html_page = ret.text
+        soup = BeautifulSoup(html_page, _DEFAULT_BEAUTIFULSOUP_PARSER)
+        section = soup.select("section.js-movie-list")
+        return loads(section[0].get("data-movies-showtimes"))
 
-    def _get(self, url, expected_status_code=200):
-        ret = self.session.get(url=url, headers=self.headers)
-        if (ret.status_code != expected_status_code):
-            raise ConnectionError(
-                'Status code {status} for url {url}\n{content}'.format(
-                    status=ret.status_code, url=url, content=ret.text))
-        return ret
+    @staticmethod
+    def _get_movies(allocine_dict):
+        """ From the Allociné dict, returns a list of Movie objects"""
+        movies = allocine_dict['movies']
+        movie_list = []
+        for movie_id, movie_details in movies.items():
+            title = movie_details.get('title')
+            movie_list.append(Movie(id=movie_id, title=title))
+        return movie_list
 
-    def _post(self, url, post_data, expected_status_code=200):
-        ret = self.session.post(url=url,
-                                headers=self.headers,
-                                data=post_data)
-        if (ret.status_code != expected_status_code):
-            raise ConnectionError(
-                'Status code {status} for url {url}\n{content}'.format(
-                    status=ret.status_code, url=url, content=ret.text))
-        return ret
+    @staticmethod
+    def _get_theater_info(allocine_dict, theater_id):
+        """ From the Allociné dict, returns a Theater object"""
+        theaters = allocine_dict.get('theaters')
+        t = theaters.get(theater_id)
+        return Theater(id=theater_id, name=t.get('name'),
+                       address=t.get('address').get('city'))
 
 
 @dataclass
@@ -48,65 +117,48 @@ class Movie:
     id: int
     title: str
 
+    def __str__(self):
+        return "{} [{}]".format(self.title, self.id)
+
+
+@dataclass
+class MovieVersion(Movie):
+    version: str  # VF, VOST
+
+    def __str__(self):
+        return "{} ({})".format(super().__str__(), self.version)
+
 
 @dataclass
 class Theater:
     id: int
     name: str
+    address: str
+
+    def __str__(self):
+        return "{} [{}] : {}".format(self.name, self.id, self.address)
 
 
 @dataclass
 class Showtime:
     datetime_str: str
+    # hour: str (see __post_init__)
+    # date: str (see __post_init__)
+    # datetime: str (see __post_init__)
+    movie: MovieVersion
 
     def __post_init__(self):
         self.datetime_obj = self._str_datetime_to_datetime_obj(
             datetime_str=self.datetime_str)
-        self.hour = self._get_hour_str(self.datetime_obj)
+        self.hour = str(self.datetime_obj.strftime("%H:%M"))
+        self.datetime = str(self.datetime_obj.strftime("%d/%m/%Y %H:%M"))
+        self.date = str(self.datetime_obj.strftime("%d/%m/%Y"))
 
-    @staticmethod
-    def _get_hour_str(datetime_obj):
-        return str(datetime_obj.strftime("%H:%M"))
+    def __str__(self):
+        return "{} {} : {}".format(self.date, self.hour, self.movie)
 
     @staticmethod
     def _str_datetime_to_datetime_obj(datetime_str,
                                       date_format=_DEFAULT_DATE_FORMAT):
         return datetime.strptime(datetime_str, date_format)
 
-
-@dataclass
-class MovieShowtimes:
-    movie: Movie
-    theater: Theater
-    showtimes: List[Showtime]
-
-
-class MyClass(object):
-    """ Class to... """
-    def __init__(self, param1, list1, dict1):
-        self.param1 = param1
-        self.list1 = list1
-        self.dict1 = dict1
-
-    def get_param1(self):
-        """ Get the param1 """
-        return(self.param1)
-
-    def __str__(self):
-        return('{}'.format(self.param1))
-
-    def __repr__(self):
-        return("Myclass(param1={})".format(self.param1))
-
-    def __len__(self):
-        return len(self.list1)
-
-    def __getitem__(self, key):
-        """ Méthod to access the object as a list
-        (ex : list1[1]) """
-        return self.list[key]
-
-    def __getattr__(self, attr):
-        """ Method to access a dictionnary key as an attribute
-        (ex : dict1.my_key) """
-        return self.dict1.get(attr, "")
--- tests/test_allocine.py
+++ tests/test_allocine.py
@@ -6,19 +6,23 @@
 # To be tested with : python3 -m pytest -vs tests/test_allocine.py
 
 import pytest
-from allocine import Movie, Theater, Showtime
+from allocine import Movie, Theater, Showtime, MovieVersion, Allocine
 
 
 def test_class_Movie():
     movie = Movie(title="Titanic", id=12345)
     assert movie.title == "Titanic"
     assert movie.id == 12345
+    print()
+    print(movie)
 
 
 def test_class_Movie_by_dict():
     movie = Movie(**{'title': 'Titanic', 'id': 12345})
     assert movie.title == "Titanic"
     assert movie.id == 12345
+    print()
+    print(movie)
 
 
 def test_class_Movie_errors():
@@ -28,16 +32,32 @@ def test_class_Movie_errors():
         Movie(id=12345)  # Missing title
 
 
+def test_class_MovieVersion():
+    movieversion = MovieVersion(title="Titanic", id=12345, version="VOST")
+    assert movieversion.title == "Titanic"
+    assert movieversion.id == 12345
+    assert movieversion.version == "VOST"
+    print()
+    print(movieversion)
+
+
 def test_class_Theater():
-    theater = Theater(name="Cinema", id=7890)
+    theater = Theater(name="Cinema", id="A7890", address="2 rue lilas, Albi")
     assert theater.name == "Cinema"
-    assert theater.id == 7890
+    assert theater.id == "A7890"
+    assert theater.address == "2 rue lilas, Albi"
+    print()
+    print(theater)
 
 
 def test_class_Theater_by_dict():
-    theater = Theater(**{'name': 'Cinema', 'id': 7890})
+    theater = Theater(**{'name': 'Cinema', 'id': 'A7890',
+                         'address': '2 rue lilas, Albi'})
     assert theater.name == "Cinema"
-    assert theater.id == 7890
+    assert theater.id == "A7890"
+    assert theater.address == "2 rue lilas, Albi"
+    print()
+    print(theater)
 
 
 def test_class_Theater_errors():
@@ -48,11 +68,30 @@ def test_class_Theater_errors():
 
 
 def test_class_Showtime():
-    showtime = Showtime(datetime_str="2018-12-15T17:15:00.000Z")
+    movieversion = MovieVersion(title="Titanic", id=12345, version="VOST")
+    showtime = Showtime(datetime_str="2018-12-15T17:15:00.000Z",
+                        movie=movieversion)
     assert showtime.datetime_str == "2018-12-15T17:15:00.000Z"
     assert showtime.hour == "17:15"
+    assert showtime.datetime == "15/12/2018 17:15"
+    assert showtime.date == "15/12/2018"
+    assert showtime.movie.title == "Titanic"
+    assert showtime.movie.id == 12345
+    assert showtime.movie.version == "VOST"
+    print()
+    print(showtime)
 
 
 def test_class_Showtime_errors():
     with pytest.raises(ValueError):
-        Showtime(datetime_str="This is not a date")
+        movieversion = MovieVersion(title="Titanic", id=12345, version="VOST")
+        Showtime(datetime_str="This is not a date", movie=movieversion)
+
+
+def test_class_Allocine():
+    a = Allocine(theater_id="P0645")
+    # print(a.movies)
+    # print(a.theater)
+    print()
+    for showtime in a.showtimes:
+        print(showtime)
